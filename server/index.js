d2f453ba912df78e52d4be6c8789e6e81398d44d
Jordan Abbasi <jordanvabbasi@gmail.com>
2021-08-13 22:12:56 -0600

    answers is now an empty object rather than null when there are no answers for a given question

--- server/index.js
+++ server/index.js
@@ -16,22 +16,33 @@ const pool = new Pool({
 
 app.get('/qa/questions', async (req, res) => {
   let client;
+  let { product_id, count, page } = req.query;
+  if (count === undefined) {
+    count = 5;
+  }
+  if (page === undefined) {
+    page = 1;
+  }
+  const offset = (page - 1) * count;
   try {
-    const { product_id } = req.query;
     client = await pool.connect();
     const query = `select q.id question_id, q.body question_body, q.date question_date, q.asker_name, q.helpfulness question_helpfulness, q.reported, (
-      select json_agg(ans)
-      from (
-        select * from answers where question_id = q.id
-      ) ans
+      coalesce(
+        (select json_object_agg(answer.id, answer)
+        from (
+          select id, body, date, answerer_name, helpfulness from answers where question_id = q.id
+        ) answer),
+        '{}'
+      )
     ) answers
-    from questions as q where q.product_id=$1`;
-    const results = await client.query(query, [product_id]);
+    from questions as q where q.product_id=$1
+    offset $2 rows
+    fetch first $3 rows only`;
+    const results = await client.query(query, [product_id, offset, count]);
     const response = {
       product_id,
       results: results.rows,
     }
-    console.log(response);
     res.status(200).send(response);
   } catch (e) {
     console.log(e);
