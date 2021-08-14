bb885a0884a911177d335a21be1f8ce7ca59f9b7
Jordan Abbasi <jordanvabbasi@gmail.com>
2021-08-13 19:29:13 -0600

    First query working - just need to fix the format

--- server/index.js
+++ server/index.js
@@ -15,11 +15,30 @@ const pool = new Pool({
 });
 
 app.get('/qa/questions', async (req, res) => {
-  const { product_id } = req.body;
-  const client = await pool.connect();
-  const results = await client.query('SELECT * FROM questions LIMIT 10');
-  client.release();
-  res.status(200).send(results);
+  let client;
+  try {
+    const { product_id } = req.query;
+    client = await pool.connect();
+    const query = `select q.id question_id, q.body question_body, q.date question_date, q.asker_name, q.helpfulness question_helpfulness, q.reported, (
+      select json_agg(ans)
+      from (
+        select * from answers where question_id = q.id
+      ) ans
+    ) answers
+    from questions as q where q.product_id=$1`;
+    const results = await client.query(query, [product_id]);
+    const response = {
+      product_id,
+      results: results.rows,
+    }
+    console.log(response);
+    res.status(200).send(response);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
+  } finally {
+    client.release();
+  }
 });
 
 app.listen(port, () => {
