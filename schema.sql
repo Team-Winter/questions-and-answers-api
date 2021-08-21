ea7f117c5275c6a77da5864fc945976ea367f544
Jordan Abbasi <jordanvabbasi@gmail.com>
2021-08-20 21:27:39 -0600

    Added slash to filepath

--- schema.sql
+++ schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE questions (
 );
 
 COPY questions (question_id, product_id, question_body, question_date, asker_name, asker_email, reported, question_helpfulness)
-FROM 'home/ubuntu/questions.csv'
+FROM '/home/ubuntu/questions.csv'
 -- FROM '/Users/jordanabbasi/HR/questions-and-answers-api/questions.csv'
 DELIMITER ','
 CSV
@@ -39,7 +39,7 @@ CREATE TABLE answers (
 );
 
 COPY answers
-FROM 'home/ubuntu/answers.csv'
+FROM '/home/ubuntu/answers.csv'
 -- FROM '/Users/jordanabbasi/HR/questions-and-answers-api/answers.csv'
 DELIMITER ','
 CSV
